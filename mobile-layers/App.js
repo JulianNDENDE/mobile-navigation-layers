95f3fdb22907d617eb5c9cfbfe12b98a089bb026
JulianNDENDE <julian.ndende@epitech.eu>
2023-07-15 20:32:19 +0200

    ✨ feat(removed comments):

--- mobile-layers/App.js
+++ mobile-layers/App.js
@@ -4,19 +4,11 @@ import AppReactNativePaper from "./AppReactNativePaper"
 import AppReactNativeTabs from "./AppReactNativeTabs"
 // import AppReactNativeBottomNav from "./AppReactNativeBottomNav"
 
-// L'une des meilleures depuis 2020
-// aujourd'hui tout le monde l'utilise
-
-// props style compliqué a gérer
 export default AppReactNavigation
 
 // export default AppReactNativePaper
 
-
-// encapsulation de react-navigation
-// complication de la configuration
 // export default AppReactNativeTabs
-
 
 // Librairie dépréciée, package non maintenu
 // export default AppReactNativeBottomNav
